d36c0565b5c3b598590c526c41e4a5a9e36fa576
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2018-08-03 14:17:12 +0200

    JPT-678 Configure Gradle

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## JPT Virtual Users
 
 This module defines generic Virtual Users mechanisms
+
+### Version and release management
+This module uses [gradle-release](https://bitbucket.org/atlassian/gradle-release/src/master/) plugin help with 
+version and release management. 
+Please refer to [plugin docs](https://bitbucket.org/atlassian/gradle-release/src/release-0.0.2/README.md) for more information.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,56 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-buildscript {
-    dependencies {
-        classpath(GradlePlugins.shadow)
-    }
-}
-
-plugins {
-    kotlin("jvm").version(Versions.kotlin)
-    maven
-}
-
-maven{
-    group = "com.atlassian.test.performance"
-    version = "0.0.1-SNAPSHOT"
-}
+val kotlinVersion = "1.2.30"
 
 val jar = tasks["jar"] as Jar
 jar.manifest.attributes["Main-Class"] = "com.atlassian.performance.tools.virtualusers.EntryPointKt";
 
-apply {
-    plugin("com.github.johnrengelman.shadow")
+plugins {
+    id("com.github.johnrengelman.shadow").version("2.0.4")
+    id("com.atlassian.performance.tools.gradle-release").version("0.0.2")
 }
 
 val shadowJar = tasks["shadowJar"] as ShadowJar
 shadowJar.isZip64 = true
 
 dependencies {
-    compile(Libs.concurrency)
-    compile(Libs.jiraActions)
-    compile(Libs.io)
-    compile(Libs.jiraSoftwareActions)
-    compile(Libs.guava)
-    compile(Libs.json)
-    compile(Libs.commonsMath3)
-    Libs.webdriver().forEach { compile(it) }
-    compile(Libs.kotlinStandard)
-    compile(Libs.csv)
-    compile(Libs.cli)
-    testCompile(Libs.junit)
+    compile("com.atlassian.performance.tools:concurrency:0.0.1")
+    compile("com.atlassian.performance.tools:jira-actions:0.0.1")
+    compile("com.atlassian.performance.tools:io:0.0.1")
+    compile("com.atlassian.performance.tools:jira-software-actions:0.1.0")
+    compile("com.google.guava:guava:23.6-jre")
+    compile("org.glassfish:javax.json:1.1")
+    compile("org.apache.commons:commons-math3:3.6.1")
+    webdriver().forEach { compile(it) }
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:$kotlinVersion")
+    compile("org.apache.commons:commons-csv:1.3")
+    compile("commons-cli:commons-cli:1.4")
+    testCompile("junit:junit:4.12")
 
-    Libs.log4jCore().forEach { compile(it) }
-    compile(Libs.webdriverManager)
-    compile(Libs.jcip)
+    log4jCore().forEach { compile(it) }
+    compile("io.github.bonigarcia:webdrivermanager:1.7.1")
+    compile("net.jcip:jcip-annotations:1.0")
 }
+
+fun webdriver(): List<String> = listOf(
+    "selenium-support",
+    "selenium-chrome-driver"
+).map { module ->
+    "org.seleniumhq.selenium:$module:3.11.0"
+} + log4j("jul")
+
+fun log4jCore(): List<String> = log4j(
+    "api",
+    "core",
+    "slf4j-impl"
+)
+
+fun log4j(
+    vararg modules: String
+): List<String> = modules.map { module ->
+    "org.apache.logging.log4j:log4j-$module:2.10.0"
+}
+
+val wrapper = tasks["wrapper"] as Wrapper
+wrapper.gradleVersion = "4.9"
+wrapper.distributionType = Wrapper.DistributionType.ALL
