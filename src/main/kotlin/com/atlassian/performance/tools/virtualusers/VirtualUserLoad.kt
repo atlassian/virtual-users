b5bbfbb56038f8662baaac48c8ce4809bea67ab2
Sebastian Pawlak <spawlak@atlassian.com>
2018-08-31 11:08:24 +0200

    PERF-16: Add whole test duration value

--- src/main/kotlin/com/atlassian/performance/tools/virtualusers/VirtualUserLoad.kt
+++ src/main/kotlin/com/atlassian/performance/tools/virtualusers/VirtualUserLoad.kt
@@ -3,8 +3,10 @@ package com.atlassian.performance.tools.virtualusers
 import java.time.Duration
 
 data class VirtualUserLoad(
-    val virtualUsers: Int,
-    val hold: Duration,
-    val ramp: Duration,
-    val load: Duration
-)
+    val virtualUsers: Int = 10,
+    val hold: Duration = Duration.ofSeconds(0),
+    val ramp: Duration = Duration.ofSeconds(0),
+    val load: Duration = Duration.ofMinutes(10)
+) {
+    val test: Duration = hold + ramp + load
+}
