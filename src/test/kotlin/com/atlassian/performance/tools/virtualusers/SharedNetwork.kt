ef8cd5870f94c126b3526023f1cce19fc9f17283
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2018-11-27 12:07:53 +0100

    Close network after the test run.

--- src/test/kotlin/com/atlassian/performance/tools/virtualusers/SharedNetwork.kt
+++ src/test/kotlin/com/atlassian/performance/tools/virtualusers/SharedNetwork.kt
@@ -3,27 +3,25 @@ package com.atlassian.performance.tools.virtualusers
 import org.junit.rules.ExternalResource
 import org.testcontainers.DockerClientFactory
 import org.testcontainers.containers.Network
-import org.testcontainers.utility.ResourceReaper
 
 internal class SharedNetwork(name: String) : ExternalResource(), Network {
-    private val id: String
-
-    init {
-        id = DockerClientFactory
-            .instance()
-            .client()
-            .listNetworksCmd()
-            .withNameFilter(name)
-            .exec()
-            .single()
-            .id
-    }
+    private val id: String = DockerClientFactory
+        .instance()
+        .client()
+        .listNetworksCmd()
+        .withNameFilter(name)
+        .exec()
+        .single()
+        .id
 
     override fun getId(): String {
-       return this.id
+        return this.id
     }
 
     override fun close() {
-        ResourceReaper.instance().removeNetworkById(id)
+        DockerClientFactory
+            .instance()
+            .client()
+            .removeNetworkCmd(id)
     }
 }
