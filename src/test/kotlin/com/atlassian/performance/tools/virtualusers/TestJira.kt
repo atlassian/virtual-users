9761b7cc2d2979d4d93b22909fbe44aa93aaf911
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-04-25 11:51:16 +0200

    JPERF-1089: Switch to a worse, but working dataset
    Generating a new dataset takes a ton of time,
    and we need to unblock CI.
    Testing on a "worse" dataset gives more condifence
    than `@Ignore` on the whole test.

--- src/test/kotlin/com/atlassian/performance/tools/virtualusers/TestJira.kt
+++ src/test/kotlin/com/atlassian/performance/tools/virtualusers/TestJira.kt
@@ -32,25 +32,9 @@ internal object TestJira {
         }
         .let { dataset -> DockerJiraFormula(Jperf424WorkaroundJswDistro("7.2.0"), dataset) }
 
-     val MANY_USERS_JIRA = URI("https://s3-eu-central-1.amazonaws.com/")
-        .resolve("jpt-custom-datasets-storage-a008820-datasetbucket-dah44h6l1l8p/")
-        .resolve("jsw-7.13.0-100k-users-sync/")
-        .let { bucket ->
-            Dataset(
-                database = MySqlDatabase(
-                    HttpDatasetPackage(
-                        uri = bucket.resolve("database.tar.bz2"),
-                        downloadTimeout = ofMinutes(6)
-                    )
-                ),
-                jiraHomeSource = JiraHomePackage(
-                    HttpDatasetPackage(
-                        uri = bucket.resolve("jirahome.tar.bz2"),
-                        downloadTimeout = ofMinutes(6)
-                    )
-                ),
-                label = "100k users"
-            )
-        }
-        .let { dataset -> DockerJiraFormula(Jperf424WorkaroundJswDistro("7.13.0"), dataset) }
+    /**
+     * We don't have a quick Jira formula with a ton of users.
+     * Blocked by [JPERF-1089](https://ecosystem.atlassian.net/browse/JPERF-1089).
+     */
+     val MANY_USERS_JIRA = SMALL_JIRA
 }
