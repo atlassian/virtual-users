8c2f6e5ca7857c48a9a6d70c0c035d29bed989ed
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-09-19 14:34:03 +0200

    JPERF-126: Pre-generate users like since 3.9.2

--- src/main/kotlin/com/atlassian/performance/tools/virtualusers/api/load/ScenarioLoadProcess.kt
+++ src/main/kotlin/com/atlassian/performance/tools/virtualusers/api/load/ScenarioLoadProcess.kt
@@ -1,5 +1,7 @@
 package com.atlassian.performance.tools.virtualusers.api.load
 
+import com.atlassian.performance.tools.concurrency.api.AbruptExecutorService
+import com.atlassian.performance.tools.concurrency.api.finishBy
 import com.atlassian.performance.tools.jiraactions.api.SeededRandom
 import com.atlassian.performance.tools.jiraactions.api.WebJira
 import com.atlassian.performance.tools.jiraactions.api.action.Action
@@ -8,47 +10,71 @@ import com.atlassian.performance.tools.jiraactions.api.memories.User
 import com.atlassian.performance.tools.jiraactions.api.memories.UserMemory
 import com.atlassian.performance.tools.jiraactions.api.memories.adaptive.AdaptiveUserMemory
 import com.atlassian.performance.tools.jiraactions.api.scenario.Scenario
+import com.atlassian.performance.tools.virtualusers.api.VirtualUserOptions
 import com.atlassian.performance.tools.virtualusers.api.browsers.Browser
 import com.atlassian.performance.tools.virtualusers.api.config.LoadProcessContainer
 import com.atlassian.performance.tools.virtualusers.api.config.LoadThreadContainer
 import com.atlassian.performance.tools.virtualusers.api.config.VirtualUserBehavior
 import com.atlassian.performance.tools.virtualusers.api.config.VirtualUserTarget
 import com.atlassian.performance.tools.virtualusers.api.diagnostics.*
-import com.atlassian.performance.tools.virtualusers.api.users.UserGenerator
 import com.atlassian.performance.tools.virtualusers.measure.ClusterNodeCounter
+import net.jcip.annotations.ThreadSafe
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import java.time.Duration
+import java.time.Instant.now
+import java.util.*
+import java.util.concurrent.Callable
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.function.Supplier
 
 class ScenarioLoadProcess : LoadProcess {
 
+    private val logger = LogManager.getLogger(this::class.java)
     private val setUpActionsRan: AtomicBoolean = AtomicBoolean(false)
 
     override fun prepareFactory(container: LoadProcessContainer): LoadThreadFactory {
         val options = container.options()
         val behavior = options.behavior
         val scenario = behavior.scenario.getConstructor().newInstance()
-        val userGenerator = behavior.userGenerator.getConstructor().newInstance()
         val browser = behavior.browser.getConstructor().newInstance()
         val counter = ClusterNodeCounter()
         container.addCloseable(AutoCloseable {
             container.result().writeNodeCounts().use { counter.dump(it) }
         })
-        return ScenarioThreadFactory(scenario, userGenerator, browser, setUpActionsRan, counter)
+        val users = preGenerateUsers(options)
+        return ScenarioThreadFactory(scenario, users, browser, setUpActionsRan, counter)
+    }
+
+    private fun preGenerateUsers(options: VirtualUserOptions): List<User> {
+        val behavior = options.behavior
+        val userGenerator = behavior.userGenerator.getConstructor().newInstance()
+        val deadline = now() + behavior.maxOverhead
+        return AbruptExecutorService(
+            Executors.newCachedThreadPool { runnable ->
+                Thread(runnable, "user-generation-${runnable.hashCode()}")
+            }
+        ).use { pool ->
+            (1..behavior.load.virtualUsers)
+                .map { pool.submit(Callable { userGenerator.generateUser(options) }) }
+                .map { it.finishBy(deadline, logger) }
+        }
     }
 }
 
+@ThreadSafe
 private class ScenarioThreadFactory(
     private val scenario: Scenario,
-    private val userGenerator: UserGenerator,
+    private val users: List<User>,
     private val browser: Browser,
     private val setUpActionRan: AtomicBoolean,
     private val nodeCounter: ClusterNodeCounter
 ) : LoadThreadFactory {
 
     private val logger: Logger = LogManager.getLogger(this::class.java)
+    private val unallocatedUsers: Queue<User> = ConcurrentLinkedQueue(users)
 
     override fun prepareThread(container: LoadThreadContainer): LoadThread {
         val options = container.loadProcessContainer().options()
@@ -63,10 +89,7 @@ private class ScenarioThreadFactory(
             target.password
         )
         val random = SeededRandom(container.random().nextLong())
-        val userMemory = AdaptiveUserMemory(random).also {
-            val generatedUser = userGenerator.generateUser(options)
-            it.remember(listOf(generatedUser))
-        }
+        val userMemory = allocateUser(random)
         val meter = container.actionMeter()
         val diagnostics = LimitedDiagnostics(
             ImpatientDiagnostics(
@@ -84,6 +107,14 @@ private class ScenarioThreadFactory(
         )
         setUpOnce(behavior, webJira, meter, target, looper)
         return ScenarioThread(webJira, looper, userLogin, nodeCounter)
+    }
+
+    private fun allocateUser(random: SeededRandom): UserMemory {
+        val memory = AdaptiveUserMemory(random)
+        val user = unallocatedUsers.poll()
+            ?: throw Exception("Asked for another user but all $users are already taken")
+        memory.remember(listOf(user))
+        return memory
     }
 
     private fun setUpOnce(
